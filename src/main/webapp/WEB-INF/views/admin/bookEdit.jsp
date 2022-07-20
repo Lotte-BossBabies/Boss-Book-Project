f7e08c6da3aef3c78b5276adf35c4dc6a63fb4b7
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-07-20 16:46:55 +0900

    feat: 관리자 상품 수정 페이지 구현 진행 #6

--- src/main/webapp/WEB-INF/views/admin/bookEdit.jsp
+++ src/main/webapp/WEB-INF/views/admin/bookEdit.jsp
@@ -1,4 +1,4 @@
-<%--
+<%@ page import="bossbabies.com.a.dto.RegisteredBookDto" %><%--
   Created by IntelliJ IDEA.
   User: user
   Date: 2022-07-20
@@ -7,10 +7,41 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>관리자 상품 수정 페이지</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="resources/js/admin.js"></script>
+</head>
+<body>
+<%
+    RegisteredBookDto dto = (RegisteredBookDto) request.getAttribute("registeredBook");
+%>
+
+<div>
+    header
+</div>
+
+<div align="center">
+    <img src="<%= dto.getImage_url()%>">
+    <p><%= dto.getTitle()%></p>
+
+    <a>재고량</a>
+    <input type="text" id="stock" name="stock" value="<%=dto.getBook_count()%>">
+
+    <br><br>
+
+    <a>할인율</a>
+    <input type="text" id="discount" name="discount" value="<%= dto.getDiscount_rate()%>">
+    <button type="button" id="updateBtn" name="updateBtn" value="<%= dto.getRegistered_book_id()%>" onclick="updateButton()">수정</button>
+    <button type="button" onclick="cancelButton()">취소</button>
+</div>
+
+
+<div>
+    footer
+</div>
+
+
+</body>
 </html>
--- src/main/webapp/resources/js/admin.js
+++ src/main/webapp/resources/js/admin.js
@@ -16,7 +16,7 @@ function getBooks(doUrl, jsonData) {
         str += "<tr>"
         str += "<td>" + '<img src="' + resp[i].image_url + '">' + "</td>"
         str += "<td>" + resp[i].title + "</td>"
-        str += "<td>" + "<button type='button' onclick='editButton()'>edit</button>" + "</td>"
+        str += "<td>" + '<button type="button" name="editBtn" id="editBtn" value="' + resp[i].registered_book_id + '" onclick="editButton()">edit</button>' + "</td>"
         str += "</tr>"
       });
 
@@ -64,9 +64,76 @@ function inputTextCheck() {
   }
 }
 
+function salesButton() {
+  var sel = document.getElementById("categorySelect");
+  var val = sel.options[sel.selectedIndex].value;
+
+  var jsonData = { "sellerId":"1", "category":val };
+
+  $.ajax({
+    url:"sales.do",
+    type:"POST",
+    data:JSON.stringify(jsonData),
+    contentType: 'application/json; charset=UTF-8',
+    dataType: 'json',
+    success: function(resp) {
+      var str = "";
+      $("#maintable").children().remove();
+
+      $.each(resp, function (i) {
+        console.log(resp[i].image_url);
+        str += "<tr>"
+        str += "<td>" + '<img src="' + resp[i].image_url + '">' + "</td>"
+        str += "<td>" + resp[i].title + "</td>"
+        str += "<td>" + resp[i].order_count + "</td>"
+        str += "<td>" + '<button type="button" name="editBtn" id="editBtn" value="' + resp[i].registered_book_id + '" onclick="editButton()">edit</button>' + "</td>"
+        str += "</tr>"
+      });
+
+      $("#maintable").append(str);
+
+    },
+    error : function () {
+      alert("error!");
+    }
+
+  });
+}
+
 function editButton() {
   var val = document.getElementById("editBtn").value;
 
+  location.href = "/bookEdit.do?registeredBookId=" + val;
 
 }
 
+function updateButton() {
+
+  var stock = document.getElementById("stock").value;
+  var discount = document.getElementById("discount").value;
+  var id = document.getElementById("updateBtn").value;
+
+  var jsonData = {"stock":stock, "discount":discount, "id":id};
+
+  $.ajax({
+    url:"update.do",
+    type:"POST",
+    data:JSON.stringify(jsonData),
+    contentType: "application/json; charset=UTF-8",
+
+    success: function (result) {
+      alert("sucess!");
+
+      location.href = "/adminMain.do?sellerId=1";
+
+    },
+    error: function () {
+      alert("error...");
+    }
+  });
+
+}
+
+function cancelButton() {
+  location.href = "/adminMain.do?sellerId=1";
+}
